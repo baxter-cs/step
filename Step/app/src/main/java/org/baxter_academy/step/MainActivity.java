3915f2ac13cb26d679ea170f9031323eb370e483
wilstenholme <woraman.lo@gmail.com>
2016-03-09 21:35:44 -0500

    Add toolbar

--- Step/app/src/main/java/org/baxter_academy/step/MainActivity.java
+++ Step/app/src/main/java/org/baxter_academy/step/MainActivity.java
@@ -3,6 +3,7 @@ package org.baxter_academy.step;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
@@ -11,6 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_layout);
+
+        // Toolbar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
     }
 
     @Override
