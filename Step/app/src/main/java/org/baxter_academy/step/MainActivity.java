0878c6335204196fb1b0ae3729b47f74d66047e3
wilstenholme <woraman.lo@gmail.com>
2016-03-16 06:59:49 -0400

    Create date and time

--- Step/app/src/main/java/org/baxter_academy/step/MainActivity.java
+++ Step/app/src/main/java/org/baxter_academy/step/MainActivity.java
@@ -4,6 +4,10 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
@@ -17,6 +21,16 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        // Dates
+        SimpleDateFormat sdfDay = new SimpleDateFormat("EEEE, MMM");
+            String timeDay = sdfDay.format(new Date());
+            TextView textDay = (TextView) findViewById(R.id.day);
+            textDay.setText(timeDay);
+        SimpleDateFormat sdfDate = new SimpleDateFormat("dd");
+            String timeDate = sdfDate.format(new Date());
+            TextView textDate = (TextView) findViewById(R.id.date);
+            textDate.setText(timeDate);
     }
 
     @Override
