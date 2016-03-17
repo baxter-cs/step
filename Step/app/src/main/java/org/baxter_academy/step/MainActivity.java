611885592d29812b5a01ac8debcd18183691f7dc
wilstenholme <woraman.lo@gmail.com>
2016-03-17 13:46:25 -0400

    Add menu buttons

--- Step/app/src/main/java/org/baxter_academy/step/MainActivity.java
+++ Step/app/src/main/java/org/baxter_academy/step/MainActivity.java
@@ -4,7 +4,12 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.SpannableString;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -31,6 +36,28 @@ public class MainActivity extends AppCompatActivity {
             String timeDate = sdfDate.format(new Date());
             TextView textDate = (TextView) findViewById(R.id.date);
             textDate.setText(timeDate);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.menu_add:
+                Toast.makeText(this, "Adding", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.menu_edit:
+                Toast.makeText(this, "Editing", Toast.LENGTH_LONG).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     @Override
